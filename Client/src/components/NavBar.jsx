0983c7dee8eec690cec84f964fd628c475d89401
Rohit Badakali <rohitbadakali7241@gmail.com>
2026-03-18 22:47:45 +0530

    feat: integrate Tailwind CSS and refactor App component - Added Tailwind CSS dependencies to package.json. - Refactored App component to remove unused code and include NavBar component.

---
+++ Client/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='h-16 bg-maintheme pt-5 pl-2 w-full'>
+      <h1 className='text-4xl'>HushBoard</h1>
+      
+    </div>
+  )
+}
+
+export default NavBar
